489b7036eb99f91e43540479d92d6ce487d1320e
FedeAlmada <feedeex46@gmail.com>
2020-05-25 04:02:02 -0300

    agrego atributos y metodo inicializarArbol()

--- ComputerPlayer.cs
+++ ComputerPlayer.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,14 +6,25 @@ namespace juegoIA
 {
 	public class ComputerPlayer: Jugador
 	{
+		private List<int> naipesHumano = new List<int>();
+		private List<int> naipesComputer = new List<int>();
+		ArbolGeneral<int> arbol = new ArbolGeneral<int>(0);
+		private int limite;		
 		
-		public ComputerPlayer()
-		{
-		}
-		
+		public ComputerPlayer(){}
+	
 		public override void  incializar(List<int> cartasPropias, List<int> cartasOponente, int limite)
 		{
-			//Implementar
+			this.naipesComputer = cartasPropias;
+			this.naipesHumano = cartasOponente;
+			this.limite = limite;				
+			bool turnoHumano = false;
+			inicializarArbol(naipesComputer, naipesHumano, limite, turnoHumano, arbol);
+		}
+		
+		public int inicializarArbol(List<int> cartasPropias, List<int> cartasOponente, int limite, bool turnoHumano, ArbolGeneral<int> raiz)
+		{
+			// Implementando...
 		}
 		
 		

ee72dd627ef0330882f52d5bff8573b749a49bee
FedericoAlmada <feedeex46@gmail.com>
2020-07-16 04:18:55 -0300

    Implemento el metodo createArbol

--- ComputerPlayer.cs
+++ ComputerPlayer.cs
@@ -5,26 +5,76 @@ using System.Linq;
 namespace juegoIA
 {
 	public class ComputerPlayer: Jugador
-	{
-		private List<int> naipesHumano = new List<int>();
-		private List<int> naipesComputer = new List<int>();
-		ArbolGeneral<int> arbol = new ArbolGeneral<int>(0);
-		private int limite;		
-		
+	{		
 		public ComputerPlayer(){}
 	
 		public override void  incializar(List<int> cartasPropias, List<int> cartasOponente, int limite)
 		{
-			this.naipesComputer = cartasPropias;
-			this.naipesHumano = cartasOponente;
-			this.limite = limite;				
-			bool turnoHumano = false;
-			inicializarArbol(naipesComputer, naipesHumano, limite, turnoHumano, arbol);
+            ArbolGeneral<int> miniMax = new ArbolGeneral<int>(0);
+            bool turnoHumano = false;
+            Estado estado = new Estado(cartasPropias, cartasOponente, limite, turnoHumano);
+			createArbol(estado, miniMax);
 		}
 		
-		public int inicializarArbol(List<int> cartasPropias, List<int> cartasOponente, int limite, bool turnoHumano, ArbolGeneral<int> raiz)
+		public int createArbol(Estado estado, ArbolGeneral<int> raiz)
 		{
-			// Implementando...
+            // Declaro una lista vacia de cartas, las cuales serán utilizadas en este metodo.
+            List<int> cartas = new List<int>();
+
+            // Si es el turno del Humano, copio sus cartas en la lista de cartas creada.
+            if(estado.getTurnoH())
+                cartas.AddRange(estado.getCartasH());
+            // Sino, copio las cartas de la Inteligencia Artificial.
+            else
+                cartas.AddRange(estado.getCartasIA());
+
+            foreach(int carta in cartas)
+            {
+                // Creo un subarbol para cada carta de la lista.
+                ArbolGeneral<int> hijo = new ArbolGeneral<int>(carta);
+
+                // Agrego cada subarbol al arbol MiniMax.
+                raiz.agregarHijo(hijo);
+                
+                // Actualizo estado nuevo de la jugada.
+                Estado nuevoEstado;
+
+                int nuevoLimite = estado.getLimite() - carta;
+                estado.setLimite(nuevoLimite);
+
+                // Creo una nueva lista de cartas y elimino la carta.
+                List<int> nuevasCartas = new List<int>();
+                nuevasCartas.AddRange(cartas);
+                nuevasCartas.Remove(carta);
+
+                // Actualizo el nuevo estado de la jugada dependiendo el turno actual.
+                if (estado.getTurnoH())
+                {
+                    bool turnoH = false;
+                    nuevoEstado = new Estado(estado.getCartasIA(), nuevasCartas, nuevoLimite, turnoH);
+                }
+                else
+                {
+                    bool turnoH = true;
+                    nuevoEstado = new Estado(nuevasCartas, estado.getCartasH(), nuevoLimite, turnoH);
+                }
+
+                // Caso base, asignación de la Función Heuristica.
+                if (estado.getLimite() < 0)
+                {
+                    if (estado.getTurnoH())
+                        hijo.setFuncionH(1);
+                    else
+                        hijo.setFuncionH(-1);
+
+                }
+                // Caso recursivo.
+                else
+                {
+                    createArbol(nuevoEstado, hijo);
+                }
+            }
+            return 0;
 		}
 		
 		

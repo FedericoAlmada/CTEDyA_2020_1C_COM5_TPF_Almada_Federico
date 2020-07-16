64b2a8989b1c192379b6e5aea85c356dae29e866
FedericoAlmada <feedeex46@gmail.com>
2020-07-16 02:44:51 -0300

    Agrego metodos y actualizo namespace

--- Estado.cs
+++ Estado.cs
@@ -7,5 +7,49 @@ namespace ProjectIA
 {
     class Estado
     {
+        private List<int> naipesHumano = new List<int>();
+        private List<int> naipesComputer = new List<int>();
+        private int limite;
+        private bool turnoHumano;
+
+        public List<int> getCartasH()
+        {
+            return naipesHumano;
+        }
+
+        public List<int> getCartasIA()
+        {
+            return naipesComputer;
+        }
+
+        public int getLimite()
+        {
+            return limite;
+        }
+
+        public bool getTurnoH()
+        {
+            return turnoHumano;
+        }
+
+        public void setCartasH(List<int> cartasH)
+        {
+            this.naipesHumano = cartasH;
+        }
+
+        public void setCartasIA(List<int> cartasIA)
+        {
+            this.naipesComputer = cartasIA;
+        }
+
+        public void setLimite(int limite)
+        {
+            this.limite = limite;
+        }
+
+        public void setTurnoH(bool turnoH)
+        {
+            this.turnoHumano = turnoH;
+        }
     }
 }

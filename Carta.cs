9b181efb6ae6cbff317096045c66a1af26cb0e81
FedericoAlmada <feedeex46@gmail.com>
2020-07-21 03:32:13 -0300

    implemento metodos y agrego atributos

--- Carta.cs
+++ Carta.cs
@@ -5,8 +5,33 @@ using System.Text;
 
 namespace juegoIA
 {
-    class Carta
+    class Carta<int><int>
     {
-        // Implementando
+        private int carta;
+        private int funcHeuristica;
+
+        public Carta(int carta, int funcHeuristica) 
+        {
+            this.carta = carta;
+            this.funcHeuristica = funcHeuristica;
+        }
+
+        public void setCarta(int carta)
+        {
+            this.carta = carta;
+        }
+        public void setFuncHeuristica(int funcHeuristica)
+        {
+            this.funcHeuristica = funcHeuristica;
+        }
+        public int getCarta()
+        {
+            return carta;
+        }
+
+        public int getFuncHeursitica()
+        {
+            return funcHeuristica;
+        }
     }
 }

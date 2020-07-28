a5ebb4e6a9698a50096497db148950fd6f3b2acd
FedericoAlmada <feedeex46@gmail.com>
2020-07-28 14:21:22 -0300

    implemento metodos

--- Consulta.cs
+++ Consulta.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ProjectIA
+namespace juegoIA
 {
     class Consulta
     {
-        // Implementado clase
+        ArbolGeneral<Carta> jugadaActualAux = new ArbolGeneral<Carta>(new Carta(0, 0));
+
+        public Consulta(ArbolGeneral<Carta> jugada)
+        {
+            this.jugadaActualAux = jugada;
+        }
+
+        public Consulta() { }
+
+        public void consultaA(ArbolGeneral<Carta> jugada) // Imprime todos los caminos que surge a partir de una raiz/jugada.
+        {
+            if (!jugada.esVacio())
+                Console.Write("(" + jugada.getDatoRaiz().getCarta() + ", " + jugada.getDatoRaiz().getFuncHeursitica() + ")");
+
+            foreach (var x in jugada.getHijos())
+            {
+                consultaA(x);
+            }
+        }
+
+        public void consultaB()
+        {
+        }
+
+        public void consultaC()
+        {
+            Console.Clear();
+            Console.WriteLine("Juego reiniciado.\n");
+            Game juego = new Game();
+            juego.play();
+        }
     }
 }
